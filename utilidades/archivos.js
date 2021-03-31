1cb31a1e26a248aec6f99ac84a345cc2b177821b
Santi Arnau <santi07a@gmail.com>
2021-03-31 09:40:50 +0200

    bugfix, creado saludo.txt

--- utilidades/archivos.js
+++ utilidades/archivos.js
@@ -1,13 +1,13 @@
+
 const fs = require("fs");
 const chalk = require("chalk");
 
-
-const guardarSaludo = (nombre) => {
-	fs.writeFile((`./ textos / saludo.txt, Hola, ${nombre}. Encantado de saludarte.`), (err) => {
+const guardarSaludo = nombre => {
+	fs.writeFile("./textos/saludo.txt", `Hola, ${nombre}. Encantado de saludarte.`, (err) => {
 		if (err) {
-			return console.log(chalk.red.bold("hay un error maldito bastardo"));
+			return console.log(chalk.red.bold("ERROR"));
 		}
 	});
 };
-module.exports = guardarSaludo;
 
+module.exports = guardarSaludo;
