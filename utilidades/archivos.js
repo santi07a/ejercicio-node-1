eb19d3c463af9f04946b6ce01fd4274264777698
Santi Arnau <santi07a@gmail.com>
2021-03-31 09:22:12 +0200

    bugfix debug, cambiado por console.log

--- utilidades/archivos.js
+++ utilidades/archivos.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 const chalk = require("chalk");
-const debug = require("debug")("http");
+
 
 const guardarSaludo = (nombre) => {
 	fs.writeFile((`./ textos / saludo.txt, Hola, ${nombre}. Encantado de saludarte.`), (err) => {
 		if (err) {
-			return debug(chalk.red.bold("hay un error maldito bastardo"));
+			return console.log(chalk.red.bold("hay un error maldito bastardo"));
 		}
 	});
 };
